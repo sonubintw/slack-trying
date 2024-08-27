e62781068e4aaa67b7f7ea2eef24a4c1d5c6447c
suraj chauhan <sonubintw@gmail.com>
2024-08-27 23:27:05 +0530

    initial commit

---
+++ src/components/UI/button.tsx
@@ -0,0 +1,11 @@
+import "./button.css"
+
+const CustomButton = (props: any): JSX.Element => {
+    return (
+        <button className="slack-button">button</button>
+    )
+}
+
+
+
+export default CustomButton
